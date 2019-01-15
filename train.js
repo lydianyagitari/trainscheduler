4fb2cc0f97238f2b1bfeb54ee03bc750bdeef5a7
Lydia Innocents <mzinnocents96@gmail.com>
2019-01-14 19:02:28 -0600

    update

--- train.js
+++ train.js
@@ -1,3 +1,4 @@
+//initializing firebase
 var config = {
     apiKey: "AIzaSyD53NG2_bOYgcPDaT9lXkD_8UAXThCkTSE",
     authDomain: "trainscheduler-d1a26.firebaseapp.com",
@@ -5,12 +6,12 @@ var config = {
     projectId: "trainscheduler-d1a26",
     storageBucket: "",
     messagingSenderId: "1046869280955"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
 
 var database = firebase.database().ref();
-//1. On click function
+//On click function for train name, destination, first train and frequency
 $("#add").on("click", function (event) {
     var Train_Name = $("#Train_Name").val();
     var destination = $("#destination").val();
@@ -19,7 +20,7 @@ $("#add").on("click", function (event) {
     writeUserData(Train_Name, destination, firstTrain, frequency)
 
 })
-//2. Write to firebase 
+// Writing to firebase 
 
 function writeUserData(Train_Name, destination, firstTrain, frequency) {
     database.push({
@@ -31,14 +32,14 @@ function writeUserData(Train_Name, destination, firstTrain, frequency) {
 }
 //3. Read from database and show
 function test() {
-    $("#rate").empty() 
+    $("#rate").empty()
     database.on("child_added", function (snapshot) {
 
-
-        var currentTime = moment(moment(), 'dd:hh:mm:ss a') 
+        //moments
+        var currentTime = moment(moment(), 'dd:hh:mm:ss a')
         var firstTrain = moment(snapshot.val().firstTrain, 'dd:hh:mm:ss a')
 
-      
+
         var frequency = snapshot.val().frequency;
         var totalMinutes = currentTime.diff(firstTrain, "minutes")
 
@@ -53,7 +54,7 @@ function test() {
         console.log("addMinutes : " + addMinutes)
         var nextTrain = moment().add(addMinutes, 'minutes').format('hh:mm a');
         console.log(nextTrain)
-
+        //intervals of train times
         $("#rate").append(("<tr> " +
             " <td> " + snapshot.val().Train_Name + " </td> " +
             " <td> " + snapshot.val().destination + " </td> " +
@@ -63,6 +64,7 @@ function test() {
 
     })
 }
+//function for start time
 function startTime() {
     var today = new moment().format('h:mm:ss a')
     // document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
